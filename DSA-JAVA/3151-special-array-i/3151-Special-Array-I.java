fda8375ac7e019bf91c5503cc115b4bccd8c3717
Sujitvinu <sujitvinurr@gmail.com>
2026-09-07 11:49:03 +0530

    [3151] [Time Beats: 58.80%] [Memory Beats: 54.96%] - LeetPush

---
+++ DSA-JAVA/3151-special-array-i/3151-Special-Array-I.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        
+        for(int i=0;i<nums.length-1;i++){
+            if((nums[i]%2==0 && nums[i+1]%2==0) || (nums[i]%2!=0 && nums[i+1]%2!=0))return false;
+        }
+       return true;
+    // if(nums.length%2==0)return false;
+    // return true;
+    }
+}
